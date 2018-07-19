077c0c620af9a663c4f5218a3ae78cdb219e71ea
Bill Baker <bill.baker@synapsewireless.com>
2018-07-19 18:54:22 -0500

    Unpacking test.

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,4 +1,4 @@
-first_name = data.get('name')
-last_name = data.get('world')
+first_name, last_name = data.get('name', 'world')
+#last_name = data.get('world')
 logger.warning("Hello {} {}".format(first_name, last_name))
 hass.bus.fire(first_name, { "wow": "from a Python script!" })
