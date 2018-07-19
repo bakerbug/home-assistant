8eda9b488859c529701cc7104457f5e4f838290d
Bill Baker <bill.baker@synapsewireless.com>
2018-07-19 18:46:00 -0500

    Fixed output.

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,3 +1,3 @@
 first_name, last_name = data.get('name', 'world')
-logger.warning("Hello {}".format(name))
+logger.warning("Hello {} {}".format(first_name, last_name))
 hass.bus.fire(name, { "wow": "from a Python script!" })
