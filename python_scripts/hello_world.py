90e9b2990644f5f24a04b40daba799e137011635
Bill Baker <bill.baker@synapsewireless.com>
2018-07-19 18:49:30 -0500

    Variable name error.

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,4 +1,4 @@
 first_name = data.get('name')
 last_name = data.get('world')
 logger.warning("Hello {} {}".format(first_name, last_name))
-hass.bus.fire(name, { "wow": "from a Python script!" })
+hass.bus.fire(first_name, { "wow": "from a Python script!" })
