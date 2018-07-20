94aa0d72c28690df4d36337848ead1ed32a00a0a
Bill Baker <bill.baker@synapse-wireless.com>
2018-07-19 22:18:08 -0500

    Tinker (#1)
    Working version of hello_world.py

--- python_scripts/hello_world.py
+++ python_scripts/hello_world.py
@@ -1,3 +1,4 @@
-name = data.get('name', 'world')
-logger.info("Hello {}".format(name))
-hass.bus.fire(name, { "wow": "from a Python script!" })
+first_name = data.get('fname')
+last_name = data.get('lname')
+logger.warning("Hello {} {}".format(first_name, last_name))
+hass.bus.fire(first_name, { "wow": "from a Python script!" })
