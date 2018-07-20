d7c51982c7edb289a0e73dac4f29800fcfb05097
Bill Baker <bakerbug@gmail.com>
2018-07-20 09:54:12 -0500

    Adds working fan control mock-up.

--- python_scripts/fan_control.py
+++ python_scripts/fan_control.py
@@ -2,7 +2,7 @@
 Adjust the ceiling fan speed based on the temperature difference between the upstairs and downstairs thermostats.
 """
 
-UPSTAIRS_TEMP = 'sensor.upstairs_thermostat_temperature'
+UPSTAIRS_TEMP_QUERY = 'sensor.upstairs_thermostat_temperature'
 DOWNSTAIRS_TEMP = 'sensor.downstairs_thermostat_temperature'
 OCCUPANCY = 'binary_sensor.bayberry_away'
 FAN_ID = 'THIS NAME NEEDS TO BE DISCOVERED ONCE SWITCH IS INSTALLED'
@@ -13,16 +13,16 @@ FAN_OFF = 0
 
 
 
-upstairs_temp = hass.states.get(UPSTAIRS_TEMP)
-downstairs_temp = hass.states.get(DOWNSTAIRS_TEMP)
-away = hass.states.get(OCCUPANCY)
+upstairs_temp = int(hass.states.get(UPSTAIRS_TEMP_QUERY).state)
+downstairs_temp = int(hass.states.get(DOWNSTAIRS_TEMP).state)
+away = hass.states.get(OCCUPANCY).state
 #fan_state = hass.states.get(FAN_ID)
 fan_state = 0
 
+
 delta = upstairs_temp - downstairs_temp
 logger.warning('Away state: {}  Upstairs: {} Downstairs: {}'.format(away, upstairs_temp, downstairs_temp))
-
-if away == 'On':
+if away == 'on':
     if delta <= 1:
         fan_speed = FAN_OFF
     elif delta == 2:
@@ -32,12 +32,7 @@ if away == 'On':
     elif delta >= 4:
         fan_speed = FAN_HIGH
 
-    if fan_state == fan_speed:
-        # Fan does not need to be adjusted
-        exit()
-    else:
+    if fan_state != fan_speed:
         # Set new fan rate.
         logger.warning('HVAC delta of {} degrees.  Setting fan to {}'.format(delta, fan_speed))
 
-else:
-    exit()
