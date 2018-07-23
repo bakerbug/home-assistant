b8e5e3db28a0a5c413309ac58b1543cf2c116765
Bill Baker <bill.baker@synapsewireless.com>
2018-07-23 08:29:07 -0500

    Removes occupancy logic from Python script.
    If this logic is desired, it can be done in HA.

--- python_scripts/fan_control.py
+++ python_scripts/fan_control.py
@@ -4,7 +4,6 @@ Adjust the ceiling fan speed based on the temperature difference between the ups
 
 entity_UPSTAIRS_THERMOSTAT = 'sensor.upstairs_thermostat_temperature'
 entity_DOWNSTAIRS_THERMOSTAT = 'sensor.downstairs_thermostat_temperature'
-entity_OCCUPANCY = 'binary_sensor.bayberry_away'
 entity_FAN = 'THIS NAME NEEDS TO BE DISCOVERED ONCE SWITCH IS INSTALLED'
 FAN_LOW = 1
 FAN_MEDIUM = 2
@@ -15,24 +14,23 @@ FAN_OFF = 0
 
 upstairs_temp = int(hass.states.get(entity_UPSTAIRS_THERMOSTAT).state)
 downstairs_temp = int(hass.states.get(entity_DOWNSTAIRS_THERMOSTAT).state)
-away = hass.states.get(entity_OCCUPANCY).state
-#fan_state = hass.states.get(entity_FAN)
+#fan_state = int(hass.states.get(entity_FAN).state)
 fan_state = 0
 
 
 delta = upstairs_temp - downstairs_temp
 logger.warning('Away state: {}  Upstairs: {} Downstairs: {}'.format(away, upstairs_temp, downstairs_temp))
-if away == 'on':
-    if delta <= 1:
-        fan_speed = FAN_OFF
-    elif delta == 2:
-        fan_speed = FAN_LOW
-    elif delta == 3:
-        fan_speed = FAN_MEDIUM
-    elif delta >= 4:
-        fan_speed = FAN_HIGH
 
-    if fan_state != fan_speed:
-        # Set new fan rate.
-        logger.warning('HVAC delta of {} degrees.  Setting fan to {}'.format(delta, fan_speed))
+if delta <= 1:
+    fan_speed = FAN_OFF
+elif delta == 2:
+    fan_speed = FAN_LOW
+elif delta == 3:
+    fan_speed = FAN_MEDIUM
+elif delta >= 4:
+    fan_speed = FAN_HIGH
+
+if fan_state != fan_speed:
+    # Set new fan rate.
+    logger.warning('HVAC delta of {} degrees.  Setting fan to {}'.format(delta, fan_speed))
 
