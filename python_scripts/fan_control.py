ed4a6b597528adcde1e19fa38b387fa46ee1eba3
Bill Baker <bill.baker@synapse-wireless.com>
2018-08-03 13:28:34 -0500

    Mqtt (#6)
    Now controls ceiling fan through MQTT and HVAC balancing.

--- python_scripts/fan_control.py
+++ python_scripts/fan_control.py
@@ -4,37 +4,45 @@ Adjust the ceiling fan speed based on the temperature difference between the ups
 
 entity_UPSTAIRS_THERMOSTAT = 'sensor.upstairs_thermostat_temperature'
 entity_DOWNSTAIRS_THERMOSTAT = 'sensor.downstairs_thermostat_temperature'
-entity_FAN = 'THIS NAME NEEDS TO BE DISCOVERED ONCE SWITCH IS INSTALLED'
-FAN_LOW = 1
-FAN_MEDIUM = 2
-FAN_HIGH = 3
-FAN_OFF = 0
+entity_FAN = 'fan.ceiling_fan'
+FAN_LOW = 'low'
+FAN_MEDIUM = 'medium'
+FAN_HIGH = 'high'
+FAN_OFF = 'off'
+FAN_ON = 'on'
 
-
-
+state_change = False
 upstairs_temp = int(hass.states.get(entity_UPSTAIRS_THERMOSTAT).state)
 downstairs_temp = int(hass.states.get(entity_DOWNSTAIRS_THERMOSTAT).state)
-#fan_state = int(hass.states.get(entity_FAN).state)
-fan_state = 0
-
-
+fan_state = hass.states.get(entity_FAN).state
+fan_speed = hass.states.get(entity_FAN).attributes["speed"]
+if fan_speed is None:
+    fan_speed = FAN_OFF
 delta = abs(upstairs_temp - downstairs_temp)
-temp_msg = 'Upstairs: {} Downstairs: {} Delta: {}'.format(upstairs_temp, downstairs_temp, delta)
-logger.info(temp_msg)
-hass.services.call('notify', 'slack_assistant', {"message":  temp_msg})
+new_state = FAN_ON
 
 if delta <= 1:
-    fan_speed = FAN_OFF
+    new_speed = FAN_OFF
+    new_state = FAN_OFF
 elif delta == 2:
-    fan_speed = FAN_LOW
+    new_speed = FAN_LOW
 elif delta == 3:
-    fan_speed = FAN_MEDIUM
+    new_speed = FAN_MEDIUM
 elif delta >= 4:
-    fan_speed = FAN_HIGH
+    new_speed = FAN_HIGH
 
-if fan_state != fan_speed:
-    # Set new fan rate.
-    fan_msg = 'HVAC delta of {} degrees.  Setting fan to {}'.format(delta, fan_speed)
+if fan_state != new_state:
+    # Toggle the fan.
+    hass.services.call('fan', 'toggle', { 'entity_id': entity_FAN})
+    state_change = True
+
+if fan_speed != new_speed:
+    # Adjust the speed.
+    hass.services.call('fan', 'set_speed', { 'entity_id': entity_FAN, 'speed': new_speed})
+    state_change = True
+
+if state_change:
+    fan_msg = 'Upstairs: {} Downstairs: {} Delta: {}.  Setting fan to {}'.format(upstairs_temp, downstairs_temp, delta, new_speed)
     logger.info(fan_msg)
     hass.services.call('notify', 'slack_assistant', {"message": fan_msg})
 
