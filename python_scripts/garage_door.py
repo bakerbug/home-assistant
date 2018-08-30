cf42703e6e9ab66b8657b077b589599348bb96bc
Bill Baker <bakerbug@gmail.com>
2018-08-30 16:45:54 -0500

    Start of cleanup work.

--- python_scripts/garage_door.py
+++ python_scripts/garage_door.py
@@ -1,13 +1,22 @@
-garage_id = data.get('entity')
-msg = "Entity is: {}.".format(garage_id)
-hass.services.call('notify', 'slack_assistant', {"message": msg})
+DEBUG = True
 
+if DEBUG:
+    debug_msg = "Script launched!"
+    hass.services.call('notify', 'slack_assistant', {"message": debug_msg})
+
+    garage_id = data.get('entity')
+    debug_data = data.get('debug')
+    debug_msg = "Received data: {}!".format(garage_id)
+    hass.services.call('notify', 'slack_assistant', {"message": debug_msg})
+
+
+garage_id = data.get('entity')
+sun = hass.states.get('sun.sun').state
 if garage_id is None:
     garage_msg = 'Garage door triggered but entity_id is None.'
 else:
     label = hass.states.get(garage_id).attributes["friendly_name"]
     state = hass.states.get(garage_id).state
-    garage_msg = '{} is {}.'.format(label, state)
+    garage_msg = '{} is {}. Sun is {}.'.format(label, state, sun)
 
-sun = hass.states.get('sun.sun').state
 hass.services.call('notify', 'slack_assistant', {"message": garage_msg})
