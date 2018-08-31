1d99ba9c5eba72c6eb5b3c87b0fccddf568e3303
Bill Baker <bill.baker@synapsewireless.com>
2018-08-30 21:05:49 -0500

    Adds garage door automation (#11)
    * Refresh garage (#10)
    
    * Cleanup (#8)
    
    * Now controls all lights triggered by sunset.  Adds Lights Out when Away.
    
    * Adds debug flag and fixes sun types.
    
    * Moved from Jinvoo to Smart Life.
    
    * Adds system notes.
    
    * Fixes group name.
    
    * Removed hello_world.py.
    
    * Slack updates now under DEBUG flag.
    
    * Explicit ignors.
    
    * Adds Docker restart notes.
    
    * Themes and User Auth (#9)
    
    * First attempt at themes.  Not working.
    
    * Adds User Auth and working Themes.
    
    * Removes theme submodule.
    
    * Start of cleanup work.
    
    * Garage door script now works.

--- python_scripts/garage_door.py
+++ python_scripts/garage_door.py
@@ -1,13 +1,26 @@
+"""
+Notify when the garage doors open or close.  Turn on the lights if they open when dark.
+"""
+
+SUN_ELEV_HIGH = 15.00
+entity_SUN = 'sun.sun'
+entity_LIGHTS = {'switch.sofa_lamp', 'group.counter_lights'}
+sun_elevation = float(hass.states.get(entity_SUN).attributes["elevation"])
 garage_id = data.get('entity')
-msg = "Entity is: {}.".format(garage_id)
-hass.services.call('notify', 'slack_assistant', {"message": msg})
 
 if garage_id is None:
-    garage_msg = 'Garage door triggered but entity_id is None.'
+    error_msg = 'Garage door triggered but entity_id is None.'
+    hass.services.call('notify', 'slack_assistant', {"message": error_msg})
+    exit()
 else:
     label = hass.states.get(garage_id).attributes["friendly_name"]
     state = hass.states.get(garage_id).state
     garage_msg = '{} is {}.'.format(label, state)
 
-sun = hass.states.get('sun.sun').state
+if sun_elevation < SUN_ELEV_HIGH and state == 'open':
+    for light_id in entity_LIGHTS:
+        hass.services.call('switch', 'turn_on', {'entity_id': light_id})
+    garage_msg = '{} is {}.  Turning on lights.'.format(label, state)
+
+
 hass.services.call('notify', 'slack_assistant', {"message": garage_msg})
