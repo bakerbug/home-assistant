0ec080535c5ba9f4aad20918749f632f57ef6d4f
Bill Baker <bakerbug@gmail.com>
2018-09-10 00:45:37 -0500

    Garage now turns on lights only when nobody home.

--- python_scripts/garage_door.py
+++ python_scripts/garage_door.py
@@ -7,6 +7,7 @@ entity_SUN = 'sun.sun'
 early_LIGHTS = 'group.evening_early_lights'
 late_LIGHTS = 'group.evening_lights'
 sun_elevation = float(hass.states.get(entity_SUN).attributes["elevation"])
+away_state = hass.states.get('binary_sensor.bayberry_away').state
 garage_id = data.get('entity')
 time = hass.states.get('sensor.time').state
 hour, minute = time.split(':')
@@ -19,16 +20,16 @@ if garage_id is None:
     exit()
 else:
     label = hass.states.get(garage_id).attributes["friendly_name"]
-    state = hass.states.get(garage_id).state
-    garage_msg = '{} is {}.'.format(label, state)
+    garage_state = hass.states.get(garage_id).state
+    garage_msg = '{} is {}.'.format(label, garage_state)
 
-if sun_elevation < SUN_ELEV_HIGH and state == 'open':
+if sun_elevation < SUN_ELEV_HIGH and garage_state == 'open' and away_state == 'on':
     if 7 < hour < 22:
         hass.services.call('switch', 'turn_on', {'entity_id': early_LIGHTS})
         hass.services.call('switch', 'turn_on', {'entity_id': late_LIGHTS})
-        garage_msg = '{} is {}.  Turning on all lights.'.format(label, state)
+        garage_msg = '{} is {}.  Turning on all lights.'.format(label, garage_state)
     elif 7 >= hour >= 22:
         hass.services.call('switch', 'turn_on', {'entity_id': late_LIGHTS})
-        garage_msg = '{} is {}.  Turning on some lights.'.format(label, state)
+        garage_msg = '{} is {}.  Turning on some lights.'.format(label, garage_state)
 
 hass.services.call('notify', 'slack_assistant', {"message": garage_msg})
