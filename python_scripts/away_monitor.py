a601b105c167c17e03738af262f6e35097e7bb9f
Bill Baker <bakerbug@gmail.com>
2018-11-03 13:02:29 -0500

    Away Monitor now inspects cell phones.

--- python_scripts/away_monitor.py
+++ python_scripts/away_monitor.py
@@ -2,19 +2,31 @@
 Enables or disables automations based on away status.
 """
 
+SEND_MSG = True
+
 away_on_list = {'automation.hvac_balancing', 'automation.lights_out_when_away', 'automation.lights_on_when_away', }
 away_off_list = {'automation.lights_on_at_early_sunset', 'automation.lights_on_at_sunset', }
 home_on_list = {'automation.lights_on_at_early_sunset', 'automation.lights_on_at_sunset', }
 home_off_list = {'automation.lights_out_when_away', 'automation.lights_on_when_away', }
-away_state = hass.states.get('binary_sensor.bayberry_away').state
+nest_away = hass.states.get('binary_sensor.bayberry_away').state
+bill_phone = hass.states.get('device_tracker.bill_cell').state
+cricket_phone = hass.states.get('device_tracker.cricket_cell').state
 
-if away_state == 'on':
+away_state = True
+if nest_away == 'off' and bill_phone == 'home' and cricket_phone == 'home':
+    away_state = False
+
+if away_state:
     for automation in away_on_list:
         hass.services.call('automation', 'turn_on', {'entity_id': automation})
     for automation in away_off_list:
         hass.services.call('automation', 'turn_off', {'entity_id': automation})
-elif away_state == 'off':
+else:
     for automation in home_on_list:
         hass.services.call('automation', 'turn_on', {'entity_id': automation})
     for automation in home_off_list:
         hass.services.call('automation', 'turn_off', {'entity_id': automation})
+
+if SEND_MSG:
+    msg = "Nest is {0}.  Bill is {1}.  Cricket is {2}.".format(nest_away, bill_phone, cricket_phone)
+    hass.services.call('notify', 'slack_assistant', {"message": msg})
