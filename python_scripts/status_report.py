9a81f3c255fb23bce7cae6652a0942f18ffe18fe
Bill Baker <bakerbug@gmail.com>
2019-11-02 08:20:23 -0500

    First version of Status Report.

---
+++ python_scripts/status_report.py
@@ -0,0 +1,32 @@
+import appdaemon.plugins.hass.hassapi as hass
+
+class StatusReport(hass.Hass):
+
+    def initialize(self):
+        self.entity_list = ("cover.left_bay", "cover.right_bay", "sensor.upstairs_thermostat_hvac_state", "sensor.downstairs_thermostat_hvac_state",
+                         "sensor.upstairs_thermostat_temperature", "sensor.downstairs_thermostat_temperature",
+                         "lock.front_door", "sensor.bill_location", "sensor.cricket_location")
+        self.alexa_list = ['media_player.kitchen', 'media_player.computer_room', 'media_player.master_bedroom']
+        status_switch = self.listen_state(self.run_report, "input_boolean.status_report", new="on")
+        self.slack("Initialized Status Report.")
+
+    def run_report(self, entity, attribute, old, new, kwargs):
+        message = self.generate_report()
+        self.send_report(message)
+
+        self.turn_off("input_boolean.status_report")
+
+    def generate_report(self):
+        report = ""
+        for entity in self.entity_list:
+            name = self.friendly_name(entity)
+            state = self.get_state(entity)
+            report = report + f"{name} is {state}. "
+        return report
+
+    def send_report(self, report_msg):
+        self.call_service("notify/alexa_media", message=report_msg, data={"type": "tts"},
+            target=self.alexa_list)
+
+    def slack(self, slack_msg):
+        self.call_service("notify/slack_assistant", message=slack_msg)
