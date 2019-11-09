e30187495d3b80d5f40adac81ce1628e358c870b
Bill Baker <bakerbug@gmail.com>
2019-11-09 10:42:00 -0600

    Prevents duplicate messages on same day.

--- python_scripts/bus_notify.py
+++ python_scripts/bus_notify.py
@@ -1,4 +1,5 @@
 import appdaemon.plugins.hass.hassapi as hass
+from datetime import date
 import smtplib
 import yaml
 from email.mime.multipart import MIMEMultipart
@@ -13,6 +14,8 @@ class BusNotify(hass.Hass):
 
         with open('/home/homeassistant/.homeassistant/secrets.yaml', 'r') as secrets_file:
             config_data = yaml.load(secrets_file)
+        self.last_date = None
+        self.debug_email = config_data["debug_email"]
         self.email_account = config_data['gmail_account']
         self.email_username = config_data['gmail_username']
         self.email_password = config_data['gmail_password']
@@ -30,27 +33,39 @@ class BusNotify(hass.Hass):
                 self.call_service('notify/slack_assistant', message=debug_msg)
 
     def send_notification(self, one, two, three, four, kwargs):
-        self.DEBUG = self.get_state('input_boolean.debug_bus_notify') == 'on'
+        current_date = date
+        if self.last_date == current_date:
+            self.slack_debug("Bus notification already sent for today.")
+            return
 
+        self.DEBUG = self.get_state('input_boolean.debug_bus_notify') == 'on'
         if self.DEBUG:
-            debug_msg = 'Sending bus notification.'
-            self.call_service('notify/slack_assistant', message=debug_msg)
+            dst_email = self.debug_email
+            msg_text = "This is a debug email.\nThere are many like it, but this one is mine."
+        else:
+            dst_email = self.email_recipients
+            msg_text = 'Good morning!\n\nKyle Baker will be riding his bicycle home today.\n\nThanks!\n-Bill'
+
+        self.last_date = current_date
+        self.slack_debug("Sending bus notification")
 
         msg = MIMEMultipart('alternative')
         msg['Subject'] = 'Kyle Baker - No Bus Today'
         msg['From'] = self.email_account
-        msg['To'] = ", ".join(self.email_recipients)
-        msg_text = 'Good morning!\n\nKyle Baker will be riding his bicycle home today.\n\nThanks!\n-Bill'
+        msg['To'] = ", ".join(dst_email)
         msg.attach(MIMEText(msg_text))
         session = smtplib.SMTP(self.smtp_server, self.smtp_port)
         session.set_debuglevel(1)
         session.starttls()
         session.login(self.email_username, self.email_password)
-        session.sendmail(self.email_account, self.email_recipients, msg.as_string())
+        session.sendmail(self.email_account, dst_email, msg.as_string())
         session.quit()
 
         self.turn_off('input_boolean.bus_notify')
 
+        self.slack_debug("Bus notification sent.")
+
+    def slack_debug(self, message):
+        self.DEBUG = self.get_state('input_boolean.debug_bus_notify') == 'on'
         if self.DEBUG:
-            debug_msg = 'Bus notification sent.'
-            self.call_service('notify/slack_assistant', message=debug_msg)
+            self.call_service("notify/slack_assistant", message=message)
