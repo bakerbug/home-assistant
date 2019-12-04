b1f3fd4f36a23494975f6e34fd510924fe0e71bc
Bill Baker <bakerbug@gmail.com>
2019-12-03 22:01:36 -0600

    More conscise feedback.

--- python_scripts/status_report.py
+++ python_scripts/status_report.py
@@ -6,6 +6,7 @@ class StatusReport(hass.Hass):
         self.entity_list = ("cover.left_bay", "cover.right_bay", "lock.front_door")
         #self.alexa_list = ['media_player.master_bedroom']
         status_switch = self.listen_state(self.run_report, "input_boolean.status_report", new="on")
+        self.insecure_states = ("open", "unlocked")
         self.slack("Initialized Status Report.")
 
     def run_report(self, entity, attribute, old, new, kwargs):
@@ -19,7 +20,10 @@ class StatusReport(hass.Hass):
         for entity in self.entity_list:
             name = self.friendly_name(entity)
             state = self.get_state(entity)
-            report = report + f"{name} is {state}. "
+            if state in self.insecure_states:
+                report = report + f"{name} is {state}. "
+        if len(report) == 0:
+            report = "The house is secure."
         return report
 
     def send_report(self, report_msg):
