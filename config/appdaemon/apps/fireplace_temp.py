970daaa2920952877eb2a0a29256dd744f92e104
Bill Baker <bill.baker@synapsewireless.com>
2021-01-04 10:47:08 -0600

    Fireplace too warm (#39)
    * Refactored to allow Alexa to control Tuya through HA.
    
    * Refactored Groups for new light names.
    
    * Adds fireplace shutdown message.
    
    * Replaces missing inputs.
    
    * Prevent announcement when temperature cools.

--- config/appdaemon/apps/fireplace_temp.py
+++ config/appdaemon/apps/fireplace_temp.py
@@ -6,8 +6,10 @@ class FireplaceTemp(hass.Hass):
     def initialize(self):
         self.max_indoor_temp_setting = "input_number.fireplace_max_indoor_temp"
         self.max_outdoor_temp_setting = "input_number.fireplace_max_outdoor_temp"
+        self.shutdown_temp = "input_number.fireplace_shutdown_temp"
         self.OUTSIDE_FIRE_TEMP = 60
         self.upstairs = "sensor.upstairs_thermostat_temperature"
+        self.upstairs_last = 0
         self.downstairs = "sensor.downstairs_thermostat_temperature"
         self.outside = "weather.khsv"
         self.fireplace_switch = "input_boolean.fireplace_temperature"
@@ -15,6 +17,7 @@ class FireplaceTemp(hass.Hass):
         self.debug_switch = "input_boolean.debug_fireplace_temp"
 
         self.request_handle = self.listen_state(self.on_request, self.fireplace_switch, new="on")
+        self.upstairs_handle = self.listen_state(self.temp_change, self.upstairs)
 
         self.yes_responses = (
             "Yes, a fire would be very nice!",
@@ -23,6 +26,7 @@ class FireplaceTemp(hass.Hass):
             "Yes, a fire sounds very cozy.",
             "Oh yes!  Light it up!",
             "Yes, just don't burn down the house.",
+            "Yes!  Light it up, watch it burn."
         )
 
         self.turn_off(self.fireplace_switch)
@@ -49,7 +53,23 @@ class FireplaceTemp(hass.Hass):
 
         self.alexa.respond(msg)
 
+    def temp_change(self, entity, attribute, old, new, kwargs):
+        upstairs_setting = self.get_state("climate.upstairs")
+        if upstairs_setting != "heat":
+            self.slack_debug("Temperature change, but the heat is not on.")
+            return
+        upstairs_temp = int(float(self.get_state(self.upstairs)))
+        shutdown_temp = int(float(self.get_state(self.shutdown_temp)))
+
+        if upstairs_temp >= shutdown_temp and upstairs_temp > self.upstairs_last:
+            msg = f"The upstairs temperature is {upstairs_temp}.  It is time to turn off the fireplace"
+            self.alexa.announce(msg, self.debug_switch)
+        else:
+            self.upstairs_last = upstairs_temp
+            self.slack_debug(f"The upstairs temperature is {upstairs_temp} and shutdown temperature is {shutdown_temp}.")
+
     def slack_debug(self, message):
         debug = self.get_state(self.debug_switch) == "on"
+        message = "(fireplace_temp) " + message
         if debug:
             self.call_service("notify/slack_assistant", message=message)
